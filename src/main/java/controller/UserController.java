bbf01872704db78794d074ec3f971080c91845e2
Samir <Samirtahirov2004@mail.ru>
2024-05-12 12:32:54 +0300

    Files are added

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,10 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/users")
+public class UserController {
+
+}
